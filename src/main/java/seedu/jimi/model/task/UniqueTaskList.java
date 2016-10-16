c3941c467a223a19396ce822a8adbe9727ce3bdc
yunxz <ih.c.frv@gmail.com>
2016-10-16 23:55:21 +0800

    substituted FloatingTask to more general ReadOnlyTask in Logic and Model package where applicable

--- src/main/java/seedu/jimi/model/task/UniqueTaskList.java
+++ src/main/java/seedu/jimi/model/task/UniqueTaskList.java
@@ -12,10 +12,10 @@ import java.util.*;
  *
  * Supports a minimal set of list operations.
  *
- * @see FloatingTask#equals(Object)
+ * @see ReadOnlyTask#equals(Object)
  * @see CollectionUtil#elementsAreUnique(Collection)
  */
-public class UniqueTaskList implements Iterable<FloatingTask> {
+public class UniqueTaskList implements Iterable<ReadOnlyTask> {
 
     /**
      * Signals that an operation would have violated the 'no duplicates' property of the list.
@@ -32,7 +32,7 @@ public class UniqueTaskList implements Iterable<FloatingTask> {
      */
     public static class TaskNotFoundException extends Exception {}
 
-    private final ObservableList<FloatingTask> internalList = FXCollections.observableArrayList();
+    private final ObservableList<ReadOnlyTask> internalList = FXCollections.observableArrayList();
 
     /**
      * Constructs empty TaskList.
@@ -57,7 +57,7 @@ public class UniqueTaskList implements Iterable<FloatingTask> {
         if (contains(toAdd)) {
             throw new DuplicateTaskException();
         }
-        internalList.add((FloatingTask) toAdd);
+        internalList.add((ReadOnlyTask) toAdd);
     }
 
     /**
@@ -75,29 +75,20 @@ public class UniqueTaskList implements Iterable<FloatingTask> {
     }
     
     /**
-     * Sets the selected task to be complete/incomplete.
+     * Replaces the task at the specified index with {@code toEdit}
      */
-    public void complete(ReadOnlyTask toComplete, boolean isComplete){
-        assert toComplete != null;
-        int targetIndex = internalList.indexOf(toComplete);
-        internalList.get(targetIndex).setCompleted(isComplete);
-    }
-    
-    /**
-     * Replaces the floating task at the specified index with {@code toEdit}
-     */
-    public void edit(int targetIndex, FloatingTask toEdit) {
+    public void edit(int targetIndex, ReadOnlyTask toEdit) {
         assert toEdit != null;
         internalList.set(targetIndex, toEdit);
     }
 
-    public ObservableList<FloatingTask> getInternalList() {
+    public ObservableList<ReadOnlyTask> getInternalList() {
         return internalList;
     }
 
     @Override
-    public Iterator<FloatingTask> iterator() {
-        Iterator<FloatingTask> i = internalList.iterator();
+    public Iterator<ReadOnlyTask> iterator() {
+        Iterator<ReadOnlyTask> i = internalList.iterator();
         return i;
     }
 
