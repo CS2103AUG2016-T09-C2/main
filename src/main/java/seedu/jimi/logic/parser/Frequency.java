c97018f2c75e4a7a83856eddc1d8c9e72a7a52b5
yunxz <ih.c.frv@gmail.com>
2016-10-28 02:58:37 +0800

    modified Frequency

--- src/main/java/seedu/jimi/logic/parser/Frequency.java
+++ src/main/java/seedu/jimi/logic/parser/Frequency.java
@@ -1,6 +1,5 @@
 package seedu.jimi.logic.parser;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Arrays;
@@ -10,7 +9,7 @@ import java.util.HashSet;
 import seedu.jimi.commons.exceptions.IllegalValueException;
 
 public class Frequency {
-    private static final String MESSAGE_INVALID_FREQUENCY = "Invalid frequency!";
+    private static final String MESSAGE_INVALID_FREQUENCY_NAME = "Invalid frequency name!";
     private static final HashSet<String> minuteEquivalents = new HashSet<>(Arrays.asList(
             "minute", "minutes", "min", "mins", "m"
     ));
@@ -19,6 +18,32 @@ public class Frequency {
     ));
     private static final HashSet<String> dayEquivalents = new HashSet<>(Arrays.asList(
             "day", "days", "d"
+    ));
+    private static final HashSet<String> mondayEquilavents = new HashSet<>(Arrays.asList(
+            "monday", "mon"
+    ));
+    private static final HashSet<String> tuesdayEquivalent = new HashSet<>(Arrays.asList(
+            "tuesday", "tu", "tue", "tues"
+    ));
+    private static final HashSet<String> wednesdayEquivalent = new HashSet<>(Arrays.asList(
+            "wednesday", "wed"
+    ));
+    private static final HashSet<String> thursdayEquivalent = new HashSet<>(Arrays.asList(
+            "thursday", "thu", "thur", "thurs" 
+    ));
+    private static final HashSet<String> fridayEquivalent = new HashSet<>(Arrays.asList(
+            "friday", "fri" 
+    ));
+    private static final HashSet<String> saturdayEquivalent = new HashSet<>(Arrays.asList(
+            "saturday", "sat"
+    ));
+    private static final HashSet<String> sundayEquivalent = new HashSet<>(Arrays.asList(
+            "sunday", "sun"
+    ));
+    private static final HashSet<String> dayOfWeekEquivalents = new HashSet<>(Arrays.asList(
+            "monday", "mon", "tuesday", "tu", "tue", "tues", "wednesday", "wed", 
+            "thursday", "thu", "thur", "thurs", "friday", "fri",
+            "saturday", "sat", "sunday", "sun"
     ));
     private static final HashSet<String> weekEquivalents = new HashSet<>(Arrays.asList(
             "week", "weeks", "w", "monday", "mon", "tuesday", "tu", "tue", "tues", 
@@ -46,16 +71,32 @@ public class Frequency {
         return dayEquivalents.contains(str);
     }
     
+    public static boolean isDayOfWeek(final String str) {
+        return dayOfWeekEquivalents.contains(str);
+    }
+    
+    public static String getDayOfWeekFull(final String str) throws IllegalValueException {
+        String lowerStr = str.trim().toLowerCase();
+        if(mondayEquilavents.contains(lowerStr))        return "MONDAY";
+        else if(tuesdayEquivalent.contains(lowerStr))   return "TUESDAY";
+        else if(wednesdayEquivalent.contains(lowerStr)) return "WEDNESDAY";
+        else if(thursdayEquivalent.contains(lowerStr))  return "THURSDAY";
+        else if(fridayEquivalent.contains(lowerStr))    return "FRIDAY";
+        else if(saturdayEquivalent.contains(lowerStr))  return "SATURDAY";
+        else if(sundayEquivalent.contains(lowerStr))    return "SUNDAY";
+        else throw new IllegalValueException("Wrong week of day input!");
+    }
+    
     public static boolean isWeek(final String str) {
-        return weekEquivalents.contains(str);
+        return weekEquivalents.contains(str.toLowerCase());
     }
     
     public static boolean isMonth(final String str) {
-        return monthEquivalents.contains(str);
+        return monthEquivalents.contains(str.toLowerCase());
     }
     
     public static boolean isYear(final String str) {
-        return yearEquivalents.contains(str);
+        return yearEquivalents.contains(str.toLowerCase());
     }
     
     public static Date getNextDate(final Date date, final int quantifier, final String freq) 
@@ -67,7 +108,7 @@ public class Frequency {
         else if(isWeek(freq))  ldt = ldt.plusWeeks(quantifier);
         else if(isMonth(freq)) ldt = ldt.plusMonths(quantifier);
         else if(isYear(freq))  ldt = ldt.plusYears(quantifier);
-        else throw new IllegalValueException(MESSAGE_INVALID_FREQUENCY);
+        else throw new IllegalValueException(MESSAGE_INVALID_FREQUENCY_NAME);
         
         return Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
     }
